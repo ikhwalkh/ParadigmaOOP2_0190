d9089fbd92409363b8f9f0edd21670b57ae44486
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-18 14:52:36 +0700

    polimorpi

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -3,7 +3,35 @@ using namespace std;
 
 class seseorang {
     public:
-         virtual void pesan() = 0;
-         // virtual void pesan() {
-         // cout <<"pesan dari seseorang" <<endl;}
+         //virtual void pesan() = 0;
+          virtual void pesan() {
+           cout <<"pesan dari seseorang" <<endl;}
 };
+
+class joko :public seseorang {
+    public:
+         void pesan() {
+            cout << "pesan dari joko" << endl;
+         }
+};
+
+class lia :public seseorang {
+        void pesan() {
+           cout << "pesan dari lia" << endl;
+        }
+};
+
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseprang::pesan();
+    return 0;
+}
