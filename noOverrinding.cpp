7ee188eff80e3322afdf953ef5a97ca97f4c4267
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-18 15:10:02 +0700

    cllass dan derivedClass

--- noOverrinding.cpp
+++ noOverrinding.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+class baseClass {
+    public:
+        virtual void perkenalan() final{
+            cout << "Halo saya Fuction dari base Class";
+        }
+};
+class derivedClass : public baseClass {
+    public:
+       void perkenalan1() {
+        cout << "Halo saya fuction dari devired Class";
+       }
+};
