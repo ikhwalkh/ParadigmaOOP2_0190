98a6674af88e24cff0bc469c7788735d7561a9fc
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-18 15:37:49 +0700

    claass rekeining bank

--- TugasPraktikum12.cpp
+++ TugasPraktikum12.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class RekeningBank {
+    protected:
+        string Ryukatsuya;
+        double saldo;
+
+    public:
+    RekeningBank(string nama, double saldoAwal)
+    {
+        
+    }
+}
